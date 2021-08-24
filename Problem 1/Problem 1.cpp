76c452f2cee96c06b7dda562221d9ab4b1103cab
Nikos <nickkaparinos@gmail.com>
2021-08-24 22:25:47 +0300

    init

---
+++ Problem 1/Problem 1.cpp
@@ -0,0 +1,18 @@
+/*
+Project Euler Problem 1
+Nick Kaparinos
+2021
+*/
+#include <iostream>
+
+using namespace std;
+int main(int argc, char* argv[]){
+    const int N = 1000;
+    int sum = 0;
+    for (int i = 0; i < N; i++) {
+        if( i%5==0 || i%3==0){
+            sum += i;
+        }
+    }
+    cout << "Result = " << sum << " !" << endl;
+}
