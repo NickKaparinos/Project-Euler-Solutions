07c6accebf22a2397064e2586ec4492bd896fb95
Nikos <nickkaparinos@gmail.com>
2022-01-10 23:35:24 +0200

    Problem 7 added

--- Problem 7/Problem 7.cpp
+++ Problem 7/Problem 7.cpp
@@ -6,33 +6,6 @@ Nick Kaparinos
 #include <iostream>
 #include <vector>
 
-//bool primeCheck(unsigned long long number);
-//
-//int main() {
-//	unsigned int counter = 1;
-//	unsigned int i = 3;
-//	while (counter <= 10000) {
-//		if (primeCheck(i)) {
-//			counter++;
-//		}
-//		i += 2;
-//	}
-//	i -= 2;
-//	std::cout << "10001th prime = " << i << std::endl;
-//}
-//
-//
-//bool primeCheck(unsigned long long number) {
-//	// Check if the number is prime
-//	bool foundFactor = false;
-//	for (unsigned long long i = 3; i <= sqrt(number); i += 2) {
-//		if (number % i == 0) {
-//			foundFactor = true;
-//			break;
-//		}
-//	}
-//	return !foundFactor;
-
 void primeCheck(std::vector<unsigned int>* primes, unsigned int number);
 
 int main(int argc, char* argv[]) {
@@ -50,6 +23,7 @@ int main(int argc, char* argv[]) {
 }
 
 void primeCheck(std::vector<unsigned int> * primes, unsigned int number) {
+	// Check if number is prime and adds it to the vector if it is
 	bool found = false;
 	for (int i : *primes) {
 		if (number % i == 0) {
